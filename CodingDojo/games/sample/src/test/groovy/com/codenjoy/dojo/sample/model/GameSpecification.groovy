0a3016f477d4e43fb29e5aebb3b0ab188761a0ac
Oleksandr_Baglai <apofig@gmail.com>
2021-04-08 00:08:20 +0300

    [sample] Refactoring in Spock test

--- CodingDojo/games/sample/src/test/groovy/com/codenjoy/dojo/sample/model/GameSpecification.groovy
+++ CodingDojo/games/sample/src/test/groovy/com/codenjoy/dojo/sample/model/GameSpecification.groovy
@@ -10,22 +10,22 @@ class GameSpecification extends Specification {
 
     AbstractGameTest game = new AbstractGameTest()
 
-    void check(String board) {
-        assert game.field() == c(board)
+    void check(String field) {
+        assert game.field() == c(field)
     }
 
-    void board(String board) {
-        game.givenFl(c(board).replace("\n", ""))
+    void field(String field) {
+        game.givenFl(c(field).replace("\n", ""))
     }
 
-    static String c(String board) {
-        int len = board.length() - StringUtils.stripStart(board, null).length()
-        return board.replaceAll("\n" + (" ".repeat(len - 1)), "\n").replaceFirst("\n", "")
+    static String c(String field) {
+        int len = field.length() - StringUtils.stripStart(field, null).length()
+        return field.replaceAll("\n" + (" ".repeat(len - 1)), "\n").replaceFirst("\n", "")
     }
 
     def "hero on the field"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -48,7 +48,7 @@ class GameSpecification extends Specification {
 
     def "hero can walk to the left"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -72,7 +72,7 @@ class GameSpecification extends Specification {
 
     def "hero can walk to the right"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -96,7 +96,7 @@ class GameSpecification extends Specification {
 
     def "hero can walk to the up"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -120,7 +120,7 @@ class GameSpecification extends Specification {
 
     def "hero can walk to the down"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -144,7 +144,7 @@ class GameSpecification extends Specification {
 
     def "if the hero does not receive commands, he does not go anywhere"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼  ☺☼
@@ -178,7 +178,7 @@ class GameSpecification extends Specification {
 
     def "hero cannot go through the border to the left"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼☼☺ ☼
@@ -202,7 +202,7 @@ class GameSpecification extends Specification {
 
     def "hero cannot go through the border to the right"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺☼☼
@@ -226,7 +226,7 @@ class GameSpecification extends Specification {
 
     def "hero cannot go through the border to the up"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼ ☼ ☼
             ☼ ☺ ☼
@@ -250,7 +250,7 @@ class GameSpecification extends Specification {
 
     def "hero cannot go through the border to the down"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -274,7 +274,7 @@ class GameSpecification extends Specification {
 
     def "hero can leave the bomb under him"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -311,7 +311,7 @@ class GameSpecification extends Specification {
 
     def "hero can leave the bomb under him and at the same moment move to the side"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -336,7 +336,7 @@ class GameSpecification extends Specification {
 
     def "there is no difference in what order the movement and the act command are executed"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -361,7 +361,7 @@ class GameSpecification extends Specification {
 
     def "hero will blow up on a bomb if he walks on it"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ x ☼
@@ -389,7 +389,7 @@ class GameSpecification extends Specification {
 
     def "hero on the field can leave as many bombs as he wants"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -418,7 +418,7 @@ class GameSpecification extends Specification {
 
     def "hero cannot leave two bombs in one cell"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺ ☼
@@ -473,7 +473,7 @@ class GameSpecification extends Specification {
 
     def "hero can pick up gold on the map, after which it will appear in a new place"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺$☼
@@ -499,7 +499,7 @@ class GameSpecification extends Specification {
 
     def "if there is no place for gold, then the program does not freeze"() {
         given:
-        board'''
+        field'''
             ☼☼☼☼☼
             ☼   ☼
             ☼ ☺$☼
